4f24f8491612adaca6c0bcd4d66e4318c3f0f8e0
Chebemeze <Chebemeze@gmail.com>
2026-07-20 17:16:21 +0100

    added csv exporter function

---
+++ csv_exporter.py
@@ -0,0 +1,16 @@
+def export_books(library: list, filename: str = "library_export.csv"):
+    if not library:
+        print("You entered an empty library. Provide a valid library")
+        return
+
+    with open(filename, "w") as f:
+        csv_list = ["title", "author", "year", "genre", "read", "rating"]
+        f.write(','.join(csv_list) + "\n")
+
+    for book in library:
+        csv_list = [book["title"], book["author"], str(book["year"]), book["genre"], str(book["read"]), str(book["rating"])]
+
+        with open(filename, "a") as f:
+            f.write(','.join(csv_list) + "\n")
+
+    print(f"📤 Successfully exported {len(library)} book(s) to {filename}!")
