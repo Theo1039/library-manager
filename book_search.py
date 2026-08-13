06a4d3c885a63ffb0b9cb8e0c84910f7be70e4ed
Golden <snrboi557@gmail.com>
2026-08-13 13:41:02 +0100

    book search bug fixed

--- book_search.py
+++ book_search.py
@@ -6,7 +6,7 @@ def booksearch(library: list) -> None:
         if search in book['title'].lower() or search in book['author'].lower():
             matches.append(book)
     if not matches:
-        print(f"No matches found for '{search}', try another word.'")
+        print(f"No matches found for '{search}', try another word.")
         return
 
     print(f"Found {len(matches)}:")
