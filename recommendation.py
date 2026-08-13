e7aa9bbd50fc8f256f56df91f43a252eca2584a4
Aguimmanuel <78384387+Aguimmanuel@users.noreply.github.com>
2026-08-13 15:28:47 +0100

    Refactor random recommendation logic in recommendation.py

--- recommendation.py
+++ recommendation.py
@@ -2,51 +2,24 @@
 import random
 #Define a random recommendation
 def random_recommendation(library: list)-> None:
+    if len(library) == 0:
+        print("📭 Your library is empty! Add some books first.")
+        return
     unread_books =[]
 # makes a list of unread books
     for book in library:
-      if not book["read"]:
-         unread_books.append(book)
+        if not book["read"]:
+            unread_books.append(book)
 
-    # checks if the user had read all the books
+# checks if the user had read all the books
     if not unread_books:
       print("HURRAY !!!!")
       print("You have read all the books in your library")
       print("You can now add more books to your library")
       return
 
-#Recomments a random book to the user
-    Recommendation = random.choice(unread_books)
-
-    print(f" {Recommendation['title']} by"
-          f" {Recommendation['author']} ({Recommendation['year']})")
-
+#recommends a random book to the user
+    recommended_book = random.choice(unread_books)
+    print(f" {recommended_book['title']} by"
+        f" {recommended_book['author']} ({recommended_book['year']})")
     print("Give this a try")
-
-
-# library = [{"title": "Dune",
-#        "author": "Frank Herbert",
-#        "year": 1965,
-#        "Genre": "fiction",
-#        "read": False
-#          },
-#        {
-#         "title": "Atomic Habits",
-#          "author": "James Clear",
-#           "year": 2018,
-#           "Genre": "fiction",
-#          "read": True
-
-#         },
-#         {
-#          "title": "The Hobbit",
-#            "author": "J.R.R.Tolkien",
-#             "year": 1937,
-#             "Genre": "fiction",
-#              "read": False
-#         }
-# ]
-# random_recommendation(library)                #integrated already, no need calling it here
-
-
-
