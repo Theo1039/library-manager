c23ec6925c9c11d0ccd9391071bc04668c87e88b
Golden <snrboi557@gmail.com>
2026-08-13 14:28:05 +0100

    book adder input bug fixed

--- book_adder.py
+++ book_adder.py
@@ -1,7 +1,11 @@
 
 def add_book(library: list):
     title = input("The name of the book is: ").strip().title()
+    if title == "":
+        print("Title cannot be empty.")
     author = input("The author of the book is: ").strip().title()
+    if author == "":
+        print("Author cannot be empty.")
     while True:
         year = input("The year of publication is: ").strip()
         if year.isdigit() and len(year) == 4:
@@ -10,6 +14,8 @@ def add_book(library: list):
         else:
             print("Invalid year. Please enter a 4-digit number.")
     genre = input("Enter genre: ").strip().title()
+    if genre == "":
+        print("Genre cannot be empty.")
 
     book = {
         "title": title,
