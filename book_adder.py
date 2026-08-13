5b3f924f1fc0cc965447b16b72709c3559a2ee71
Golden <snrboi557@gmail.com>
2026-08-13 15:48:01 +0100

    book adder year bug fixed2

--- book_adder.py
+++ book_adder.py
@@ -7,12 +7,16 @@ def add_book(library: list):
     if author == "":
         print("Author cannot be empty.")
     while True:
-        year = input("The year of publication is: ").strip()
+        year_input = input("The year of publication is: ").strip()
+        try:
+            year = int(year_input)
+        except ValueError:
+            print("Please enter a valid 4-digit number.")
+            continue
         if 1000 <= year <= 2026:
-            year = int(year)
             break
         else:
-            print("Please enter a year between 1000 and 2026.")
+            print("Please enter a year between 1000 and 2026")
     genre = input("Enter genre: ").strip().title()
     if genre == "":
         print("Genre cannot be empty.")
